bdd26ef437e559558a24b8b8b83bc087bdfd2e41
zhudeyi <2634718650@qq.com>
2021-04-01 09:25:46 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -2,12 +2,15 @@
 
 
 namespace App\Http\Controllers\Test;
-
+use Xieheng\Love\Consts\Common\UserConst;
 
 class TestController
 {
     public function index()
     {
+        $a = UserConst::STATUS_DRAFT;
+        $b = UserConst::test();
+        dd($b);
         dd(123);
     }
 }
